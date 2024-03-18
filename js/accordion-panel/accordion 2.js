976f5fb7d89f6dcda8ea527c83752f33ee71d339
GitHub <action@github.com>
2024-03-18 20:18:24 +0000

    Correct CI for package build and splitting

---
+++ js/accordion-panel/accordion 2.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var accordion = function accordion() {
+  $('.js-accordion').accordion();
+};
+//# sourceMappingURL=accordion 2.js.map
---
+++ js/patternlab.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* 
+ * code specific to patternlab goes here
+ */
+
+$(document).ready(function () {
+  // Grid toggle behavior, dev only
+  $('.gridToggle').on('click', function () {
+    $('body').toggleClass('layout-grid--on');
+  });
+  // Include labels
+  $('.includeToggle').on('click', function () {
+    $('body').toggleClass('twig-includes--on');
+  });
+
+  // Initialize RIA components; 
+  // this is handled differently in Drupal
+  // apax_megamenu();
+
+  image_gallery();
+  accordion();
+  ambient_video();
+  carousel();
+  modals();
+  togglebutton();
+  responsive_grid_table();
+  resourcesMenu();
+  globalMegamenu();
+  //link_group_dropdown();
+
+});
+//# sourceMappingURL=patternlab.js.map
